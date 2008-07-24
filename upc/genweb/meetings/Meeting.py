7a3ade27db18af34d5411ae3327a3caf705d3651
carles.bruguera <carles.bruguera@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-07-24 13:10:50 +0000

    Update dels camps a richedit event modificacio
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genweb.meetings@154 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genweb/meetings/Meeting.py
+++ upc/genweb/meetings/Meeting.py
@@ -38,31 +38,46 @@ schema = Schema((
             description_msgid='meetings_help_date',
             i18n_domain='meetings',
         ),
+        label="Date",
     ),
     LinesField(
         name='atendees',
         widget=AJAXAddRemoveWidget(
             label="Atendees",
+            allow_add=0,
+            description="Search and select who is invited to the meeting. An e-mail will be sent to all the atendees.",
             label_msgid='meetings_label_atendees',
+            description_msgid='meetings_help_atendees',
             i18n_domain='meetings',
         ),
         vocabulary='getUsers',
+        label="Atendees",
     ),
     TextField(
         name='agenda',
-        widget=TextAreaWidget(
-            label='Agenda',
+        allowable_content_types=('text/plain', 'text/structured', 'text/html', 'application/msword',),
+        widget=RichWidget(
+            label="Agenda",
+            description="What is going to be discussed during the meeting",
             label_msgid='meetings_label_agenda',
+            description_msgid='meetings_help_agenda',
             i18n_domain='meetings',
         ),
+        default_output_type='text/html',
+        label="Agenda",
     ),
     TextField(
         name='acta',
-        widget=TextAreaWidget(
-            label='Acta',
+        allowable_content_types=('text/plain', 'text/structured', 'text/html', 'application/msword',),
+        widget=RichWidget(
+            label="Acta",
+            description="Briefing of the meeting once concluded",
             label_msgid='meetings_label_acta',
+            description_msgid='meetings_help_acta',
             i18n_domain='meetings',
         ),
+        default_output_type='text/html',
+        label="Acta",
     ),
 ),
 )
@@ -88,7 +103,7 @@ class Meeting(BaseContent, BrowserDefaultMixin):
     schema = Meeting_schema
 
     ##code-section class-header #fill in your manual code here
-    
+
     security.declarePublic('getUsersList')
     def getUsersList(self):
         """
@@ -96,13 +111,23 @@ class Meeting(BaseContent, BrowserDefaultMixin):
         from Products.CMFCore.utils import getToolByName
         au = getToolByName(self, 'acl_users')
         
-        ld_atendees = [au.LDAP.acl_users.searchUsers(cn=ld,ou='users',dc='upc')[0] for ld in self.getAtendees()]
-        users_list = []
-        for u in ld_atendees:
-            mail = 'mail' in u.keys() and u['mail'] or ''
-            users_list.append((u['cn'],u['sn'],mail))
-        return users_list
-    
+        ld_atendees = []
+        for ld in self.getAtendees():
+            mail = ''
+            fullname = ld
+            user = au.getUserById(ld)
+            if 'LDAP' in user._propertysheets.keys():
+                ps = user.getPropertysheet('LDAP')
+                mail = ps.getProperty('email')
+                fullname = ps.getProperty('fullname')
+            ld_atendees.append((ld,fullname,mail))
+        return ld_atendees
+
+
+    ##/code-section class-header
+
+    # Methods
+
     security.declarePublic('getUsers')
     def getUsers(self):
         """
@@ -111,9 +136,9 @@ class Meeting(BaseContent, BrowserDefaultMixin):
         au = getToolByName(self, 'acl_users')
         return DisplayList(tuple(self.getUsersList()))
 
-    ##/code-section class-header
+    # Manually created methods
 
-    # Methods
+
 
 
 registerType(Meeting, PROJECTNAME)
--- upc/genweb/meetings/events.py
+++ upc/genweb/meetings/events.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 def meeting_created(meeting, event):
     assert meeting == event.object # At least normally, see below
     host = meeting.MailHost
@@ -23,9 +24,40 @@ Aquesta és una notificació automàtica per comunicar-li que ha estat convidat 
 
 %s
 """ % (from_email.encode('UTF-8'),
-       user[2],
+       user[2].encode('UTF-8'),
        meeting.Title(),
        meeting.absolute_url()                      )
 
-    host.send(mail_text)
+                host.send(mail_text)
+
+def meeting_edited(meeting, event):
+    assert meeting == event.object # At least normally, see below
+    host = meeting.MailHost
+    
+    import pdb;pdb.set_trace()
+    from Products.CMFCore.utils import getToolByName
+    au = getToolByName(meeting, 'acl_users')
+    owner = au.getUserById(meeting.owner_info()['id'])
+    from_email = ''
+    if 'LDAP' in owner._propertysheets.keys():
+        ps = owner.getPropertysheet('LDAP')
+        if 'email' in ps._properties.keys():
+            from_email = ps.getProperty('email')
+    if from_email:   
+        for user in meeting.getUsersList():
+            if user[2]:
+                mail_text = """From:%s
+To: %s
+Subject: Invitació a reunió
+Content-Type: text/plain; charset=UTF-8
+
+Aquesta és una notificació automàtica per comunicar-li que s'han produit canvis en la reunió %s a la qual esta convidat/ada.Pot consultar els detalls en aquest enllaç 
+
+%s
+""" % (from_email.encode('UTF-8'),
+       user[2].encode('UTF-8'),
+       meeting.Title(),
+       meeting.absolute_url()                      )
+
+                host.send(mail_text)
 
