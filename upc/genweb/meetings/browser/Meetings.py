9859846afc644d054375b481e92e4afb604c3302
carles.bruguera <carles.bruguera@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-11-14 07:08:37 +0000

    Fixed hardcoded nom del ldap plugin
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genweb.meetings@465 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genweb/meetings/browser/Meetings.py
+++ upc/genweb/meetings/browser/Meetings.py
@@ -20,7 +20,12 @@ class Atendees(BrowserView):
         from Products.CMFCore.utils import getToolByName
         filter=self.request.get('filter','')
         au = getToolByName(self, 'acl_users')
-        ldap_users = au.LDAP.acl_users.searchUsers(cn=filter,ou='users',dc='upc')
+        ldap_plugins = [item[1] for item in au.items() if item[1].meta_type=="Plone LDAP plugin"]
+        if ldap_plugins:
+            LDAP = ldap_plugins[0]
+            ldap_users = LDAP.acl_users.searchUsers(cn=filter,ou='users',dc='upc')
+        else:
+            ldap_users = []
         if ldap_users:
             if ldap_users[0]['dn']=='Too many results for this query':
                 return {'result':'error','type':'too_many'}
