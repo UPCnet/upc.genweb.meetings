048f932fbed1cefa3b94dc5ad643ad6dc3bcad99
victor.fernandez <victor.fernandez@6deaf63b-a510-49cf-aad4-8c651776ecce>
2009-01-30 11:14:31 +0000

    definitive!
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genweb.meetings@787 6deaf63b-a510-49cf-aad4-8c651776ecce

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '1.0'
 
 setup(name='upc.genweb.meetings',
       version=version,
-      description="",
+      description="Paquet del contingut reunions",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
